455fa449516b51d2888d07637b4e9215583f5eb7
Matthew Cieslak <sanibelmatt7@aol.com>
2015-01-04 18:46:35 -0500

    working on distance finding function

--- appserv.js
+++ appserv.js
@@ -23,6 +23,7 @@ server.listen(8080);
 
   var users = [];
   var userData = [];
+  var available = [];
 
    io.sockets.on('connection', function(socket){
 
@@ -40,13 +41,31 @@ function makeid()
 
     return text;
 }
+function randomize(username){
+  //find nearest user
+  var best;
+  var self_lat = users[username].lat;
+  var self_long = users[username].longi;
+  if(available.length > 1){
+    //console.log("available array length" + available.length);
+    console.log("me : " + username + " lat:" + self_lat + " long: " + self_long);
+    for(var x=0; x<available.length; x++){
+      //sort by distance
+        /*  var distLong = self_long - available[x].longi;
+          var distLat = self_lat - available[x].lat;
+          var res = distLong + distLat;
+          */
+
+      console.log("distance from " + available[x].username + " long:" + available[x].longi + " lat:" + available[x].lat);
+    }
+  }
+}
 socket.on("check-in", function(username, lat, longi){
       	var user = {
          		lat: lat,
          		longi: longi,
          		username: username,
          	}
-
           	var id = makeid();
              socket.user = id
              socket.username = username;
@@ -57,7 +76,10 @@ socket.on("check-in", function(username, lat, longi){
              users.push(socket.username);
              userData.push(user);
                		console.log("array len: " + users.length);
+                   available.push(user);
+                   randomize(username);
                		socket.broadcast.emit("update-map", userData);
+
         });
 
 

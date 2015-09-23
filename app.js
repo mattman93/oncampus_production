1cbfe7030161194e395c75ba64734702fb2fcc85
mattman93 <sanibelmatt7@aol.com>
2015-09-23 15:48:35 -0400

    reconfig

--- app.js
+++ app.js
@@ -293,7 +293,7 @@ socket.on("send_shout", function(from, msg, isAdmin){
 });
 
 socket.on("get_all_shouts", function(){
-  connection.query("SELECT * FROM messages ORDER BY(id) DESC LIMIT 150",
+  connection.query("SELECT * FROM messages ORDER BY(id) ASC LIMIT 150",
       function selectCb(err, results){
       if(err){ throw err; }
        recurse_results(results, 0);
